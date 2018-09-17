c2cede491ae060b54a14f8d222e012096892223c
Lara Chappuie <20chappuie@stfrancishs.org>
2018-09-17 20:08:08 +0000

    morefun

--- 42while24.c
+++ 42while24.c
@@ -2,21 +2,25 @@
 int main(void)
 {
     int sum=0;
-    int k;
+    int k=0;
 
-    for (k=0;k<5;k++)
+    //for (k=0;k<5;k++)
+    while (k<5)
     {
         sum+=k; //use something different in "while" version
+        k++;
+        printf ("k - %2d sum - %4.1d\n",k,sum);
     }
 
-    printf ("%d\n",sum);
 
-    k=5;
 
-    while (k>0) {
-        k=k-1; //use something different in "for" version
-        printf ("%d\n", k);
+    //k=5;
+
+    //while (k>0) {
+    for (k=10;k>0;k--)
+    {     //use something different in "for" version
+        printf ("%6.1d\n", k);
     }
-
+    printf ("Blastoff!");
 }
 
